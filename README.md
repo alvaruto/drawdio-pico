da55c1714b4d2a927463cf18932e728c0924cd4c
alvaruto <alvaro.martinez24@alu.uclm.es>
2021-03-23 16:21:43 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # drawdio-pico
 Drawdio implementation using Raspberry Pi Pico
+
+## What is drawdio?
+Drawdio is a musical instrument based on impedance measurement. In this repository, you will find an implementation for the Raspberry Pi Pico, which features
+a RP2040 microcontroller.
+
+## How do I build it?
+You only need:
+- A RPi Pico
+- 1MOhm resistor
+- A buzzer
+- A pencil
+- Some wires
+
+This program reads the voltage from a voltage divider made of the resistor and your own body, and plays a mapped voltage to the buzzer pin.
---
+++ src/main.c
@@ -0,0 +1,56 @@
+/******************************
+ * main.c
+ * rev 1.0 Jan 2021 shabaz
+ * ****************************/
+
+#include "pico/stdlib.h"
+#include <stdio.h>
+#include "hardware/pwm.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+
+const uint buzzer = 4;
+const uint signal = 28;
+const float conversion_factor = 3.3f / (1 << 12);
+uint16_t result = 0;
+float voltage;
+float level;
+
+int main(void)
+{
+    //Initialize everything needed
+    stdio_init_all();
+
+    adc_init();
+    adc_gpio_init(signal);
+    //GPIO28 uses ADC2
+    adc_select_input(2);
+
+    // Tell the buzzer pin that the PWM is in charge of its value.
+    gpio_set_function(buzzer, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(buzzer);
+    pwm_config config = pwm_get_default_config();
+    // Load the configuration into our PWM slice, and set it running.
+    pwm_init(slice_num, &config, true);
+    
+    while(true){
+        result = adc_read();
+        voltage = result * conversion_factor;
+
+        if(result < 1862){
+            //Below 1.5V, output is 0
+            level = 0;
+        }
+        else if(result > 3223){
+            //From 3V, output is 0
+            level = 0;
+        }
+        else{
+            //Between 1.5 and 3V, the output is linear
+            level = voltage*170 - 255; 
+        }
+        pwm_set_gpio_level(buzzer,level);
+        printf("%f\n",voltage);
+    }
+
+}
